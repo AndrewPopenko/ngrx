d55dcb390a97026f627cbcc69e5d5ae243a2a173
AndrewPopenko <andrew.popenko@gmail.com>
2024-01-29 23:56:59 +0100

    cleaning

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -14,26 +14,10 @@ import { AsyncPipe, NgIf } from "@angular/common";
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-// export class CounterOutputComponent implements OnInit, OnDestroy {
 export class CounterOutputComponent {
   count$: Observable<number>;
-  // counter = 0;
-  // counterServiceSub?: Subscription;
 
-  // constructor(private counterService: CounterService) {}
   constructor(private store: Store<{counter: number}>) {
     this.count$ = this.store.select('counter')
   }
-
-  // ngOnInit(): void {
-  //   this.counterServiceSub = this.counterService.counterChanged.subscribe(
-  //     (newVal) => (this.counter = newVal)
-  //   );
-  // }
-  //
-  // ngOnDestroy(): void {
-  //   if (this.counterServiceSub) {
-  //     this.counterServiceSub.unsubscribe();
-  //   }
-  // }
 }
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,8 +1,4 @@
-import { createReducer } from "@ngrx/store";
-
 const initialStateOfCounter = 0;
-
-// export const counterReducer = createReducer(initialStateOfCounter);
 
 export function counterReducer(state = initialStateOfCounter): number {
     return state;
