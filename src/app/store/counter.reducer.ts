090eecb582eb35f7dbb47bfab0da8617d8d76cc7
AndrewPopenko <andrew.popenko@gmail.com>
2024-02-10 00:21:20 +0100

    actions and reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,5 +1,12 @@
+import { createReducer, on } from "@ngrx/store";
+import { increment } from "./counter.actions";
+
 const initialStateOfCounter = 0;
 
-export function counterReducer(state = initialStateOfCounter): number {
-    return state;
-}
+export const counterReducer = createReducer(
+    initialStateOfCounter, on(increment,(state, action) => state + action.value));
+
+
+// export function counterReducer(state = initialStateOfCounter): number {
+//     return state;
+// }
